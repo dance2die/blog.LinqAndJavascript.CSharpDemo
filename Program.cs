bb9ecfb6d75c11c4c30de9dffe25dfe64df3a2d9
Sung Kim <sungmkim80@gmail.com>
2018-04-14 16:48:33 -0400

    fixed typo

--- Program.cs
+++ Program.cs
@@ -72,7 +72,7 @@ namespace LinqAndJavascript.CSharpDemo
             PrintHeaderFooter("Last/LastOrDefault DEMO - Get Last Order", () => LastDemo(Orders));
             PrintHeaderFooter("DefaultIfEmpty DEMO - Get Order or Default if Empty", () => DefaultIfEmptyDemo(Orders));
             PrintHeaderFooter("Skip/SkipWhile DEMO - Skip Orders", () => SkipDemo(Orders));
-            PrintHeaderFooter("Take/TakeWhile DEMO - Skip Orders", () => TakeDemo(Orders));
+            PrintHeaderFooter("Take/TakeWhile DEMO - Take Orders", () => TakeDemo(Orders));
         }
 
         private static void TakeDemo(List<Order> orders)
